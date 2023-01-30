b66fb8685043a1f1792375a844bf042c4475776d
Baraka M Mulumia <65751779+barakamulumia@users.noreply.github.com>
2023-01-30 12:20:33 +0300

    feat: db & prisma set up with routes

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,19 @@ import morgan from 'morgan';
 import Express from 'express';
 
 import path from 'path';
+import router from './router';
 
 const app = Express();
 app.use(cors());
 app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'static/index.html'));
 });
+
+app.use('/api', router);
 
 app.listen(8080, () => {
   InfoLogger('Creativity starts with viewing the whole world differently');
