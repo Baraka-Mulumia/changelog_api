8f65f764ff46609716c07f71f52c56e515cc8abb
Baraka Mshindi Mulumia <65751779+barakamulumia@users.noreply.github.com>
2023-02-02 06:34:21 +0300

    Delete constants.ts

--- src/config/constants.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-export const DEFAULT_TIME_FORMAT_PATTERN = 'do MMM yyyy HH:mm:ss';
-export const DEFAULT_TIME_FORMAT_PATTERN_SHORT = 'do MMMM yyyy';
-
-export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'secret';
